f8221ca8dbde6c94d15bc4f08866a302d870e593
Alan Z <zheng.alan.work@gmail.com>
2025-10-26 01:48:32 -0700

    Started implementing multiple object detection

--- main.py
+++ main.py
@@ -8,13 +8,30 @@ SUITCASE   = 28
 LAPTOP     = 63
 CELL_PHONE = 67
 
-CLASS_FILTER = [PERSON, BACKPACK, HANDBAG, SUITCASE, LAPTOP, CELL_PHONE]
+CLASS_FILTER = [PERSON, BACKPACK, SUITCASE, LAPTOP, CELL_PHONE]
 TRACKER      = "botsort.yaml"
 MODEL_PATH   = "yolo11m.pt"
 
 model = YOLO(MODEL_PATH)
 initPosX = -100
 initPosY = -100
+suitcaseMap = {}
+phoneMap = {}
+backpackMap = {}
+laptopMap = {}
+
+def getMap(n):
+    if n == SUITCASE:
+        return suitcaseMap
+    elif n == CELL_PHONE:
+        return phoneMap
+    elif n == LAPTOP:
+        return laptopMap
+    elif n == BACKPACK:
+        return backpackMap
+
+
+
 trackedObjectID = -10
 started = False
 def getTrackID(box):
@@ -26,41 +43,61 @@ def getTrackID(box):
 
 # Iterate the tracker stream once (this opens and reads the webcam internally)
 for r in model.track(
-    source=0,                 # webcam index
+    source=1,                 # webcam index
     device="cuda:0",
     half= True,
     tracker=TRACKER,
     persist=True,             # keep IDs across frames
     stream=True,              # generator: yields one Results per frame
-    conf = 0.3,
+    conf = 0.4,
     classes=CLASS_FILTER,     # filter classes
     verbose=False
 ):
+    laptopIDX = 0
+    backpackIDX = 0
+    phoneIDX = 0
+    suitcaseIDX = 0
     frame = r.plot()  # annotated image with boxes/ids/labels
     if(not started):
         for box in r.boxes:
-            if int(box.cls) == SUITCASE:
+            val = int(box.cls) 
+            if val != PERSON:
                 trackedObjectID = int(box.id)
                 started = True
                 initPosX, initPosY, w, h = box.xywh[0].cpu().numpy().astype(int)
                 print(initPosX)
                 print(initPosY)
-                print("SUITCASE HAS BEEN PLANTED")
+                objStr = ""
+                if val == SUITCASE:
+                    suitcaseMap[suitcaseIDX] = [initPosX,initPosY,SUITCASE]
+                    objStr = "SUITCASE"
+                    suitcaseIDX+=1
+                elif val == LAPTOP:
+                    laptopMap[laptopIDX] = [initPosX, initPosY, LAPTOP]
+                    objStr = "LAPTOP"
+                    laptopIDX += 1
+                elif val == CELL_PHONE:
+                    phoneMap[phoneIDX] = [initPosX, initPosY, CELL_PHONE]
+                    objStr = "CELL PHONE"
+                    phoneIDX+=1
+                elif val == BACKPACK:
+                    backpackMap[backpackIDX] = [initPosX, initPosY, BACKPACK]
+                    objStr = "CELL PHONE"
+                    backpackIDX+=1
+                print(objStr + " HAS BEEN PLANTED")
     idFound = False
     for box in r.boxes:
-        if getTrackID(box) == trackedObjectID or int(box.cls) == SUITCASE:
-            found = True
-            x,y,w,h = box.xywh[0].cpu().numpy().astype(int)
-            if(getTrackID(box) != trackedObjectID):
-                print("Changed IDs")
-                trackedObjectID = getTrackID(box)
-            offset = 20
-            if initPosX-offset < x < initPosX+offset and initPosY-offset < y < initPosY+offset:
-                print("NOT MOVED")
-            else:
-                print("OBJECT MOVED")
-
-
+        classes = int(box.cls)
+        
+        idFound = True
+        x,y,w,h = box.xywh[0].cpu().numpy().astype(int)
+        offset = 20
+        if initPosX-offset < x < initPosX+offset and initPosY-offset < y < initPosY+offset and 0 <= x <= 640 and 0<=y<=480:
+            print("NOT MOVED")
+        else:
+            print("OBJECT MOVED")
+    if not idFound:
+        print("OBJECT MOVED")
     
 
                 
